c293d0b834e1421ccfef7c0f11aa0b19d0a54c05
Frank Hernandez <frank@axomtechnologies.com>
2022-04-17 19:03:50 -0400

    feat: :sparkles: adds user model
    adds entity to user module

---
+++ src/users/user.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Table,
+  Column,
+  Model,
+  Unique,
+  IsEmail,
+  DataType,
+  CreatedAt,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'user',
+})
+export class User extends Model<User> {
+  @Column({
+      type: DataType.UUID,
+      defaultValue: DataType.UUIDV4,
+      primaryKey: true,
+  })
+  id: string;
+
+  @Unique
+  @IsEmail
+  @Column
+  email: string;
+
+  @Column
+  password: string;
+
+  @Column({ field: 'first_name' })
+  firstName: string;
+
+  @Column({ field: 'last_name' })
+  lastName: string;
+
+  @CreatedAt
+  @Column({ field: 'created_at' })
+  createdAt: Date;
+
+  @UpdatedAt
+  @Column({ field: 'updated_at' })
+  updatedAt: Date;
+}
