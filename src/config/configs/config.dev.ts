3ef6d6171bad0069251b02355e140e434b9063c5
Frank Hernandez <frank@axomtechnologies.com>
2022-04-17 20:58:47 -0400

    feat: :sparkles: swagger setup
    setups swagger to work /docs

--- src/config/configs/config.dev.ts
+++ src/config/configs/config.dev.ts
@@ -3,11 +3,11 @@ import { Dialect } from 'sequelize/types';
 export const config = {
     database: {
         dialect: 'postgres' as Dialect,
-        host: 'localhost',
-        port: 5432,
-        username: 'hello',
-        password: 'world',
-        database: 'papa',
+        host:  process.env.DB_HOST || 'localhost',
+        port: Number(process.env.PORT) || 5432,
+        username: process.env.DB_USER || 'frankpersonal',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_DATABASE || 'postgres',
         logging: false,
     }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const PORT = process.env.port || 3000;
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    setupSwagger(app);
+    await app.listen(PORT);
 }
+
 bootstrap();
